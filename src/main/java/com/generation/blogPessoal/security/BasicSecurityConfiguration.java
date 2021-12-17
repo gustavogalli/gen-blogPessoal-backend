ef8a100bbeb9d8756e190080978bdb8a890932ad
Gustavo Galli <gustavosgalli@gmail.com>
2021-12-17 12:22:43 -0300

    @gustavogalli [Task: refatorando camada Security]

--- src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
+++ src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
@@ -2,43 +2,44 @@ package com.generation.blogPessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-   
-	@Autowired
-	private UserDetailsService userDetailsService;
+public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-		
-		auth.inMemoryAuthentication().withUser("boaz").password(passwordEncoder().encode("boaz"))
-		.authorities("ROLE_ADMIN");
-	}
-	
+	private @Autowired UserDetailsServiceImplements service;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service);
+	
+		auth.inMemoryAuthentication().withUser("root")
+				.password(passwordEncoder().encode("458italia"))
+				.authorities("ROLE_ADMIN");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().cors()
-		.and().csrf().disable();
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/usuarios").permitAll()
+				.antMatchers(HttpMethod.PUT, "usuarios/credentials").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().cors()
+				.and().csrf().disable();
 	}
+
 }
