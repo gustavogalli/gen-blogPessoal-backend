1733a69cfd3babc94562ef86e4d18d9a056bcd23
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-27 19:50:59 -0300

    Refatorando BasicSecurityConfiguration

--- src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
+++ src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
@@ -32,18 +32,17 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-        	.antMatchers("/**").permitAll()
-        	.antMatchers("/usuarios/logar").permitAll()
-        	.antMatchers("/usuarios/cadastrar").permitAll()
-        	.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
-        	.antMatchers(HttpMethod.GET ,"/tema").permitAll()
-        	.antMatchers(HttpMethod.POST ,"/tema").permitAll()
-        	.anyRequest().authenticated()
-        	.and().httpBasic()
-        	.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        	.and().cors()
-        	.and().csrf().disable();
-	}
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+        .antMatchers("/**").permitAll()
+        .antMatchers("/usuarios/logar").permitAll()
+        .antMatchers("/usuarios/cadastrar").permitAll()
+        .antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.GET ,"/tema").permitAll()
+        .anyRequest().authenticated()
+        .and().httpBasic()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().cors()
+        .and().csrf().disable();
+    }
 }
