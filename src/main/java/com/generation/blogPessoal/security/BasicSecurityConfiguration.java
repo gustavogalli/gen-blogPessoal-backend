f09b91eb35f80f4b141a1c7505e6e83347e99ec4
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-27 16:33:16 -0300

    Refatorando BasicSecurityConfiguration

--- src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
+++ src/main/java/com/generation/blogPessoal/security/BasicSecurityConfiguration.java
@@ -33,16 +33,16 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/usuarios/cadastrar").permitAll()
-				.antMatchers(HttpMethod.POST, "/usuarios/logar").permitAll()
-				.antMatchers(HttpMethod.GET, "/postagem").permitAll()
-				.antMatchers(HttpMethod.GET, "/tema").permitAll()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().cors()
-				.and().csrf().disable();
+        http.authorizeHttpRequests()
+        	.antMatchers("/**").permitAll()
+        	.antMatchers("/usuarios/logar").permitAll()
+        	.antMatchers("/usuarios/cadastrar").permitAll()
+        	.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        	.antMatchers(HttpMethod.GET ,"/tema").permitAll()
+        	.anyRequest().authenticated()
+        	.and().httpBasic()
+        	.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        	.and().cors()
+        	.and().csrf().disable();
 	}
 }
